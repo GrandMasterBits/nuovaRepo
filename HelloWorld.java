510a618b63a88f54bc5f35bf6118d0822835685c
GrandMasterBits <petru.sili@studio.unibo.it>
2024-01-20 11:33:55 +0100

    Modified HelloWork.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public final class HelloWorld {
 	private static final String AUTHOR = "Danilo Pianini";
 
 	public static void main(final String[] args) {
+		
 		System.out.println("This program has been realised by " + AUTHOR);
 	}
 
